f54585360bb5c4fe6c30175490018988ecb7140b
JGChristiansen <dvg554@alumni.ku.dk>
2021-09-13 00:02:11 +0200

    fixed a mistake in evalErr

--- Assignments/Assignment 1/code/part2/src/Arithmetic.hs
+++ Assignments/Assignment 1/code/part2/src/Arithmetic.hs
@@ -66,17 +66,12 @@ evalErr (Cst x) _ = Right x
 evalErr (Add e1 e2) env = do {x <- evalErr e1 env ; y <- evalErr e2 env ; return (x + y)}
 evalErr (Sub e1 e2) env = do {x <- evalErr e1 env ; y <- evalErr e2 env ; return (x - y)}
 evalErr (Mul e1 e2) env = do {x <- evalErr e1 env ; y <- evalErr e2 env ; return (x * y)}
-evalErr (Div e1 e2) env = do 
-                          x <- evalErr e2 env
-                          if x == 0 then Left EDivZero
-                          else do {y <- evalErr e1 env ; return (div y x)}
-evalErr (Pow e1 e2) env = do 
-                          x <- evalErr e2 env
-                          if x < 0 then Left ENegPower
-                          else do {y <- evalErr e1 env ; return (y ^ x)}       
-evalErr (If test yes no) env = do
-                               x <- evalErr test env
-                               if x /= 0 then evalErr yes env else evalErr no env
+evalErr (Div e1 e2) env = do {y <- evalErr e1 env ; x <- evalErr e2 env ;
+                          if x == 0 then Left EDivZero else return (div y x)}
+evalErr (Pow e1 e2) env = do {y <- evalErr e1 env ; x <- evalErr e2 env ;
+                          if x < 0 then Left ENegPower else return (y ^ x)}
+evalErr (If test yes no) env = do {x <- evalErr test env ;
+                               if x /= 0 then evalErr yes env else evalErr no env}
 evalErr (Var variableName) env = case env variableName of
                                     Nothing -> Left (EBadVar variableName)
                                     Just a -> Right a
