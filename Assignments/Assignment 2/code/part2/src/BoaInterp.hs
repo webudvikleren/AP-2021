2651f2661fca6eddf87747d2a8cbd99cbb362aef
JGChristiansen <dvg554@alumni.ku.dk>
2021-09-16 19:11:27 +0200

    added function operate

--- Assignments/Assignment 2/code/part2/src/BoaInterp.hs
+++ Assignments/Assignment 2/code/part2/src/BoaInterp.hs
@@ -58,7 +58,28 @@ truthy (StringVal s) = s /= []
 truthy (ListVal xs) = xs /= []
 
 operate :: Op -> Value -> Value -> Either String Value
-operate = undefined
+operate Plus (IntVal x) (IntVal y) = Right (IntVal (x+y))
+operate Plus _ _ = Left "Cannot add non-integer values."
+operate Minus (IntVal x) (IntVal y) = Right (IntVal (x-y))
+operate Minus _ _ = Left "Cannot subtract non-integer values."
+operate Times (IntVal x) (IntVal y) = Right (IntVal (x*y))
+operate Times _ _ = Left "Cannot multiply non-integer values."
+operate Div (IntVal x) (IntVal y) = if y == 0 then Left "Division by zero."
+                                    else Right (IntVal (div x y))
+operate Div _ _ = Left "Cannot divide non-integer values."
+operate Mod (IntVal x) (IntVal y) = if y == 0 then Left "Modulo by zero"
+                                    else Right (IntVal (mod x y))
+operate Mod _ _ = Left "Cannot modulo non-integer values."
+operate Eq x y = if x == y then Right TrueVal else Right FalseVal
+operate Less (IntVal x) (IntVal y) = if x < y then Right TrueVal
+                                     else Right FalseVal
+operate Less _ _ = Left "Cannot compare (<) non-integer values."
+operate Greater (IntVal x) (IntVal y) = if x > y then Right TrueVal
+                                        else Right FalseVal
+operate Greater _ _ = Left "Cannot compare (>) non-integer values."
+operate In x (ListVal xs) = if x `elem` xs then Right TrueVal
+                            else Right FalseVal
+operate In _ _ = Left "Cannot check membership of non-list value."
 
 apply :: FName -> [Value] -> Comp Value
 apply = undefined
