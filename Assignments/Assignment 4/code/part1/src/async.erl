b172a185430a8445eb7ea214ddc5f57c2069f7c6
Mathias Gammelgaard <webudviklerendk@gmail.com>
2021-09-30 16:30:05 +0200

    A4

--- Assignments/Assignment 4/code/part1/src/async.erl
+++ Assignments/Assignment 4/code/part1/src/async.erl
@@ -2,7 +2,51 @@
 
 -export([new/2, wait/1, poll/1]).
 
-new(Fun, Arg) -> nope.
-wait(Aid) -> nope.
-poll(Aid) -> nope.
+new(Fun, Arg) -> spawn(fun() -> 
+    Me = self(),
+    process_flag(trap_exit, true),
+    spawn_link(fun() ->
+      Res = Fun(Arg),
+      Me ! {comp_done, Res}
+    end),
+    loop({true, false, nothing})
+  end
+).
 
+wait(Aid) -> 
+  Aid ! {self(), get_state},
+  receive
+    {Working, _, _} ->
+      if
+        Working -> wait(Aid);
+        true -> ok
+      end
+  end.
+
+poll(Aid) ->
+  Aid ! {self(), get_state},
+  receive
+    {Working, Success, Res} ->
+      if
+        Working -> nothing;
+        Success -> {ok, Res};
+        true -> {exception, Res}
+      end
+  end.
+
+loop({Working, Success, Res} = State) ->
+  receive
+    {comp_done, _Res} -> 
+      io:format("Hej verden"),
+      loop({false, true, _Res});
+    {'EXIT', Worker, Reason} ->
+      io:format(Reason),
+      if
+        Reason /= "normal" ->
+          loop({false, false, Reason}),
+          io:format("Fejl")
+      end;
+    {From, get_state} ->
+      From ! State,
+      loop(State)
+  end.
