c77c496acb72368a0269b0ecf79b9a9723cfe174
Mathias Gammelgaard <webudviklerendk@gmail.com>
2021-10-03 10:10:19 +0200

    fixed search

--- Assignments/Assignment 4/code/part2/src/emoji.erl
+++ Assignments/Assignment 4/code/part2/src/emoji.erl
@@ -8,16 +8,41 @@
 -type emoji() :: binary().
 -type analytic_fun(State) :: fun((shortcode(), State) -> State).
 
+-spec start([{string(), binary()}]) -> any().
+start(Initial) ->
+  case length(Initial) == sets:size(sets:from_list(Initial)) of
+    true -> spawn(fun() ->
+      loop({dict:from_list(Initial), dict:new()})  
+    end);
+    false -> {error, "Initial elements contains duplicates"}
+  end.
 
-start(_) -> not_implemented.
+-spec new_shortcode(string(), string(), binary()) -> any().
+new_shortcode(E, Short, Emo) -> 
+  Me = self(),
+  E ! {register, Me, Short, Emo},
+  receive
+    RetVal -> RetVal
+  end.
 
-new_shortcode(_, _, _) -> not_implemented.
+-spec alias(string(), string(), string()) -> any().
+alias(E, Short1, Short2) -> 
+  Me = self(),
+  E ! {alias, Me, Short1, Short2},
+  receive
+    RetVal -> RetVal
+  end.
 
-alias(_, _, _) -> not_implemented.
+-spec delete(string(), string()) -> any().
+delete(E, Short) -> E ! {delete, Short}.
 
-delete(_, _) -> not_implemented.
-
-lookup(_, _) -> not_implemented.
+-spec lookup(string(), string()) -> any().
+lookup(E, Short) ->
+  Me = self(),
+  E ! {lookup, Me, Short},
+  receive
+    RetVal -> RetVal
+  end.
 
 analytics(_, _, _, _, _) -> not_implemented.
 
@@ -26,3 +51,94 @@ get_analytics(_, _) -> not_implemented.
 remove_analytics(_, _, _) -> not_implemented.
 
 stop(_) -> not_implemented.
+
+loop({Shortcodes, Alias} = State) ->
+receive
+  {register, From, Short, Emo} ->
+    case dict:is_key(Short, Shortcodes) of
+      true -> From ! {error, "Short already exists"};
+      false -> From ! ok, loop({dict:append(Short, Emo, Shortcodes), Alias})
+    end;
+  {alias, From, Short1, Short2} ->
+    case dict:is_key(Short1, Shortcodes) of
+      true -> From ! ok, loop({Shortcodes, dict:append(Short1, Short2, Alias)});
+      false -> case dict_search_for_val(Short1, Alias) of
+        true -> From ! ok, loop({Shortcodes, dict:append(Short1, Short2, Alias)});
+        false -> From ! {error, "Short1 not registered"}
+      end
+    end;
+  {delete, Short} ->
+    loop({dict:erase(Short, Shortcodes), dict_delete(Short, Alias)});
+  {lookup, From, Short} ->
+    case dict:is_key(Short, Shortcodes) of
+      true -> 
+        Emo = dict:fetch(Short, Shortcodes),
+        From ! {ok, Emo},
+        loop(State);
+      false -> case dict_search(Short, Alias) of
+        Short1 ->
+          case dict:is_key(Short1, Shortcodes) of
+            true -> 
+              Emo = dict:fetch(Short1, Shortcodes),
+              From ! {ok, Emo};
+            false -> From ! no_emoji
+          end
+      end
+    end,
+    loop(State)
+end.
+
+dict_delete(Short, Dict) -> 
+  case dict:is_key(Short, Dict) of 
+    true ->
+      Values = dict:fetch(Short, Dict),
+      Dict1 = lists:foldr(fun(S, Acc) -> dict_delete(S, Acc) end, Dict, Values),
+      io:format("~p", [Short]),
+      dict:erase(Short, Dict1);
+    false -> dict:erase(Short, Dict)
+  end.
+  
+dict_search(Short, Dict) -> 
+  Keys = dict:fetch_keys(Dict),
+  Mapped = lists:map(fun(Key) ->
+    Values = dict:fetch(Key, Dict),
+    lists:map(fun(Value) -> 
+      if
+        Short == Value -> dict_search(Key, Dict);
+        true -> false
+      end
+    end, Values)
+  end, Keys),
+  Concatted = lists:concat(Mapped),
+  Filtered = lists:filter(fun(Elm) -> Elm /= false end, Concatted),
+  if
+    length(Filtered) == 0 -> Short;
+    true -> lists:nth(1, Filtered)
+  end.
+
+dict_search_for_val(Short, Dict) -> 
+  Keys = dict:fetch_keys(Dict),
+  Mapped = lists:map(fun(Key) ->
+    Values = dict:fetch(Key, Dict),
+    lists:map(fun(Value) -> 
+      if
+        Short == Value -> dict_search(Key, Dict);
+        true -> false
+      end
+    end, Values)
+  end, Keys),
+  Concatted = lists:concat(Mapped),
+  Filtered = lists:filter(fun(Elm) -> Elm /= false end, Concatted),
+  length(Filtered) /= 0.
+
+
+
+% delete(Short 2)
+
+%Shortcode:
+% Short 1 -> Emoji
+
+% Alias:
+%Short 1 -> [Short 2]
+%Short 2 -> [Short 3, SHort 4]
+%Short 3 -> [Short 5]
