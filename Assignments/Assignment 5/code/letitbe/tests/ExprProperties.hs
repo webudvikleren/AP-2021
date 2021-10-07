c168dbd947073a37e531f001cc864f1c43223fb6
JGChristiansen <dvg554@alumni.ku.dk>
2021-10-07 15:56:50 +0200

    added haskell part of A5

--- Assignments/Assignment 5/code/letitbe/tests/ExprProperties.hs
+++ Assignments/Assignment 5/code/letitbe/tests/ExprProperties.hs
@@ -4,10 +4,26 @@ import Test.QuickCheck
 
 import ExprAst
 import qualified ExprEval as E
-
+import Data.List (intersect, isInfixOf)
 
 instance Arbitrary Expr where
-   arbitrary = undefined
+   arbitrary = expr
+
+-- from slides
+expr :: Gen Expr
+expr = sized exprN
+
+exprN :: Int -> Gen Expr
+exprN 0 = fmap Const arbitrary
+exprN n = oneof [ fmap Const arbitrary
+                 , fmap Var arbitrary
+                 , Oper Plus <$> subexpr <*> subexpr
+                 , Oper Minus <$> subexpr <*> subexpr
+                 , Oper Times <$> subexpr <*> subexpr
+                 , Let "x" <$> subexpr `suchThat` (not . isInfixOf "Var" . show) <*> subexpr]
+          where subexpr = exprN (n `div`2)
 
 prop_eval_simplify :: Expr -> Property
-prop_eval_simplify x = x === x
+prop_eval_simplify x = E.eval (E.simplify x) mempty === E.eval x mempty
+
+-- generate [char]
