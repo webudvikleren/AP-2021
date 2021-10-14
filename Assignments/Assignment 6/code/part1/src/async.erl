35d39334fdd63affb17aada161dac614faddfcf2
JGChristiansen <dvg554@alumni.ku.dk>
2021-10-14 12:13:24 +0200

    added assignment 6

---
+++ Assignments/Assignment 6/code/part1/src/async.erl
@@ -0,0 +1,9 @@
+-module(async).
+
+-export([new/2, wait/1, poll/1, wait_catch/1, wait_any/1]).
+
+new(Fun, Arg) -> nope.
+wait(Aid) -> nope.
+poll(Aid) -> nope.
+wait_catch(Aid) -> nope.
+wait_any(Aids) -> nope.
