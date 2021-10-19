0abb92c80b88ca2580deb69737be52b8d62190f1
JGChristiansen <dvg554@alumni.ku.dk>
2021-10-19 11:52:08 +0200

    finished warm-up

--- Assignments/Assignment 6/code/part1/src/async.erl
+++ Assignments/Assignment 6/code/part1/src/async.erl
@@ -1,92 +1,84 @@
 -module(async).
--behaviour(gen_server).
+-behaviour(gen_statem).
 
-
+-export([init/1, callback_mode/0, handle_event/4, terminate/3, code_change/4]).
 -export([new/2, wait/1, poll/1, wait_catch/1, wait_any/1]).
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3]).
+-export([working/3, notWorking/3]).
 
 % =============================================================================
 %                              API FUNCTIONS
 % =============================================================================
 
-new(Fun, Arg) -> gen_server:start(?MODULE, {Fun, Arg}, []).
+new(Fun, Arg) -> gen_statem:start(?MODULE, {Fun, Arg}, []).
 
-wait(Aid) -> case gen_server:call(Aid, call, infinity) of
-                  nothing -> wait(Aid);
-                  {ok,Res} -> Res;
-                  {exception,Ex} -> throw(Ex)
+poll(Aid) -> gen_statem:call(Aid, poll).
+
+wait(Aid) -> case gen_statem:call(Aid, wait) of
+                {ok, Res} -> Res;
+                {exception, Ex} -> throw(Ex)
              end.
-
-poll(Aid) -> gen_server:call(Aid, call, infinity).
-
-wait_catch(Aid) -> case gen_server:call(Aid, call, infinity) of
-                        nothing -> wait_catch(Aid);
+            
+wait_catch(Aid) -> case gen_statem:call(Aid, wait) of
                         {ok, Res} -> {ok, Res};
                         {exception, Ex} -> {exception, Ex}
-                   end.
+                    end.
 
-%% Wait any with spawn. Do not know how to get the caught value out of lists:foreach
-%% it just return "ok.".
-wait_any(Aids) -> lists:foreach(fun(Aid) -> spawn(fun() ->
-                        catch case gen_server:call(Aid, call, infinity) of
-                            nothing -> wait_any([Aid]);
-                            {ok, Res} -> io:format("Res"), throw({Aid, Res});
-                            {exception, Ex} -> io:format("Ex"), throw(Ex)
-                         end
-                         end) end, Aids).
-
-%% The return value here is correct, but it waits for all processes to finish,
-%% before it returns which is wrong. Maybe spawn a process for each Aid?
-%% throw(something), does not throw an exception, just local return which we can
-%% catch.
-wait_any_(Aids) -> catch lists:foreach(fun(Aid) ->
-                         case gen_server:call(Aid, call, infinity) of
-                            nothing -> wait_any([Aid]);
-                            {ok, Res} -> throw({Aid, Res});
-                            {exception, Ex} -> throw(Ex)
-                         end
-                         end, Aids).
+wait_any(Aids) ->
+    Ref = make_ref(),
+    Me = self(),
+    lists:foreach(fun(Aid) -> spawn(fun() ->
+                                    Res = gen_statem:call(Aid, wait),
+                                    Me ! {Aid, Ref, Res}
+                                    end) end, Aids),
+    receive
+        {Aid, Ref, {ok, Res}} -> {Aid, Res};
+        {_, Ref, {exception, Ex}} -> throw(Ex)
+    end.
 
 % =============================================================================
 %                              CALLBACK FUNCTIONS
 % =============================================================================
 
+%% The state can only be changed by the worker. From working to notWorking.
+working(cast, {worker, EvalRes}, _Data) ->
+    {next_state, notWorking, EvalRes};
 
-handle_call(call, _From, State) ->
-    case State of
-       {true,_,_} -> {reply, nothing, State};
-       {false,true,Res} -> {reply, {ok, Res}, State};
-       {false,false,Ex} -> {reply, {exception, Ex}, State}
-    end;
+working({call, From}, poll, _Data) ->
+    {keep_state_and_data, [{reply, From, nothing}]};
 
-handle_call({worker,State}, _From, _) ->
-    case State of
-        {false, true, Res} -> {reply, Res, {false, true, Res}};
-        {false, false, Reason} -> {reply, Reason, {false, false, Reason}}
-    end.
+working({call, _From}, wait, _Data) ->
+        {keep_state_and_data, [postpone]}.
+
+notWorking({call, From}, poll, Data) ->
+        {keep_state_and_data, [{reply, From, Data}]};
+
+notWorking({call, From}, wait, Data) ->
+        {keep_state_and_data, [{reply, From, Data}]}.
 
 init(_Args={Fun, Arg}) ->
-        Me = self(),
-        spawn(fun() ->
-                   try
-                     Res = Fun(Arg),
-                     gen_server:call(Me, {worker, {false, true, Res}})
-                   catch
-                     _:Reason -> gen_server:call(Me,
-                                 {worker, {false, false, Reason}})
-                  end end),
-        {ok, {true, false, nothing}}.
+    Me = self(),
+    spawn(fun() ->
+               try
+                 Res = Fun(Arg),
+                 gen_statem:cast(Me, {worker, {ok, Res}})
+               catch
+                 _:Reason -> gen_statem:cast(Me, {worker, {exception, Reason}})
+              end end),
+    Data = {ok, noRes},
+    {ok, working, Data}.
 
-% dummy implementations to satisfy behaviour.
-handle_cast(_Msg, State) ->
-    {noreply, State}.
+callback_mode() ->
+    state_functions.
 
-handle_info(_Info, State) ->
-    {noreply, State}.
+%% The last 4 functions are interface functions to satisfy behaviour.
+handle_event(enter, _OldState, _State, _Data) ->
+    keep_state_and_data;
 
-terminate(_Reason, _State) ->
+handle_event(_EventType, _EventContent, _State, _Data) ->
+    keep_state_and_data.
+
+terminate(_Reason, _State, _Data) ->
     ok.
 
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
+code_change(_OldVsn, State, Data, _Extra) ->
+    {ok, State, Data}.
